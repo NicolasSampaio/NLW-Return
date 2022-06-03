d28b545436092542dd6a44bc4e42bb531484376b
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-06-02 21:41:09 -0300

    apiRequestLimiter

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,25 +5,15 @@ import { routes } from "./routes";
 const app = express();
 const rateLimit = require("express-rate-limit");
 
-// const apiRequestLimiter = rateLimit({
-//   windowMs: 1 * 60 * 1000, // 1 minute
-//   max: 2, // limit each IP to 2 requests per windowMs
-//   handler: function (req: express.Request, res: express.Response) {
-//     return res.status(429).json({
-//       error: "You sent too many requests. Please wait a while then try again",
-//     });
-//   },
-// });
-
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "https://nicolassampaioreturn.vercel.app"
-//   );
-//   res.header("Access-Control-Allow-Methods", "POST");
-//   app.use(cors());
-//   next();
-// });
+const apiRequestLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // limit each IP to 2 requests per windowMs
+  handler: function (req: express.Request, res: express.Response) {
+    return res.status(429).json({
+      error: "You sent too many requests. Please wait a while then try again",
+    });
+  },
+});
 
 app.use(
   cors({
@@ -31,8 +21,7 @@ app.use(
     methods: ["POST"],
   })
 );
-// app.use(cors());
-// app.use(apiRequestLimiter);
+app.use(apiRequestLimiter);
 app.use(express.json());
 app.use(routes);
 
